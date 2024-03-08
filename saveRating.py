6e1754b65f70a82f5d82fb01b72b43d3bb5d695f
Serkan <serkanbay02@hotmail.com>
2024-03-08 18:51:36 +0300

    update saveRating: don't feedback user if not registered

--- saveRating.py
+++ saveRating.py
@@ -1,9 +1,10 @@
 import json
 import requests
 
+
 def saveRating(pollId, votedBy, isVoted, votedOption, API_URL, TOKEN):
     url = f"{API_URL}/saveRating"
-    
+
     data = {
         "pollId": pollId,
         "votedBy": votedBy,
@@ -14,15 +15,15 @@ def saveRating(pollId, votedBy, isVoted, votedOption, API_URL, TOKEN):
 
     json_string = json.dumps(data)
 
-    response = requests.post(url, json = json_string)
-    
+    response = requests.post(url, json=json_string)
+
     print("Response from /saveRating: ", response.json())
 
 
-    if "error" in response.json():
-        error = response.json()["error"]
-        if error == "userHasNotFound":
-            return "Değerlendirmeniz kaydedilmedi, yemeklerin iyileşmesine yardımcı olmak için lütfen sitemize kaydolun. ☺️"
-        return "Değerlendirmeniz kaydedilemedi, lütfen daha sonra tekrar deneyin."
+#    if "error" in response.json():
+#        error = response.json()["error"]
+#        if error == "userHasNotFound":
+#            return "Değerlendirmeniz kaydedilmedi, yemeklerin iyileşmesine yardımcı olmak için lütfen sitemize kaydolun. ☺️"
+#        return "Değerlendirmeniz kaydedilemedi, lütfen daha sonra tekrar deneyin."
 
     return "Değerlendirmeniz kaydedildi! Teşekkürler. ☺️"
